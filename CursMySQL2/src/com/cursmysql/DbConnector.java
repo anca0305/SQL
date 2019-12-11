ba3b49435f94f90bf9fb17acb8c0702776dff670
anca0305 <57723527+anca0305@users.noreply.github.com>
2019-12-11 19:17:44 +0200

    Add files via upload
    In pauza

---
+++ CursMySQL2/src/com/cursmysql/DbConnector.java
@@ -0,0 +1,26 @@
+package com.cursmysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class DbConnector {
+    private static Connection connection = null;
+
+    public static Connection getDbConnection() {
+        String url = "jdbc:mysql://localhost:3306/facultate";
+        String pass = "";
+        String user = "root";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, pass);
+            System.out.println(connection);
+            System.out.println("Successfully connected to database.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
---
+++ CursMySQL2/src/com/cursmysql/Main.java
@@ -0,0 +1,11 @@
+package com.cursmysql;
+
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Students students = null;
+       students.showStudents();
+    }
+}
